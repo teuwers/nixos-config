c54deb89483b9a676e4c0c10b4a0344c495cebfa
Teuwers <lrd2wers@yandex.ru>
2022-12-04 13:15:55 +0300

    update

--- postinstall-nb.sh
+++ postinstall-nb.sh
@@ -8,4 +8,7 @@ mkdir -p ~/.repos
 ln -s /etc/nixos ~/.repos/nixos-config
 echo "file:///home/teuwers/.repos Repos" >> ~/.config/gtk-3.0/bookmarks
 
+mkdir -p ~/.yandex-disk
+echo "file:///home/teuwers/.yandex-disk Yandex.Disk" >> ~/.config/gtk-3.0/bookmarks
+echo "Name Yandex.Disk folder .yandex-disk"
 yandex-disk setup
