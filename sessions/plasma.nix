7e93ce6ee7ae26aaaacdbe58529f50d7808715dc
teuwers <lrd2wers@yandex.ru>
2024-04-04 17:45:15 +0300

    up

--- sessions/plasma.nix
+++ sessions/plasma.nix
@@ -9,6 +9,7 @@
   services.xserver.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    package = pkgs.kdePackages.sddm;
   };
   
   programs.kdeconnect.enable = true;
@@ -24,6 +25,25 @@
     kdePackages.kdialog
     kdePackages.plasma-integration
     kdePackages.plasma-browser-integration
+    kdePackages.kcmutils
+    kdePackages.sddm-kcm
+    kdePackages.kio
+    kdePackages.kgpg
+    kdePackages.kleopatra
+    kdePackages.kdav
+    kdePackages.kmail
+    kdePackages.kcalc
+    kdePackages.ktouch
+    kdePackages.kontact
+    kdePackages.skanpage
+    kdePackages.kdenlive
+    kdePackages.korganizer
+    kdePackages.kio-extras
+    kdePackages.plasma-disks
+    kdePackages.plasma-vault
+    kdePackages.kaddressbook
+    kdePackages.print-manager
+    smplayer
     config.nur.repos.baduhai.koi
   ];  
 }
